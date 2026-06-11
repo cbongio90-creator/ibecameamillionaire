6ebf9d92bf81cd30a373f19abb237aafd511af50
cbongio90-creator <c.bongio90@gmail.com>
2026-06-11 23:56:26 +0200

    Update total.js

--- netlify/functions/total.js
+++ netlify/functions/total.js
@@ -1,6 +1,20 @@
+const { getStore } = require("@netlify/blobs");
+
 exports.handler = async () => {
+  const store = getStore("donations");
+
+  const total = await store.get("total");
+  const count = await store.get("count");
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ total: 0 })
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({
+      total: Number(total || 0),
+      count: Number(count || 0)
+    })
   };
 };
